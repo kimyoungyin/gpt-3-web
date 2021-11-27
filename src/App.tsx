b2dc35805abd1dd48b6bc2370e99ef3bbda7bf2e
kimyoungyin <yin199859@gmail.com>
2021-11-27 17:17:50 +0900

    feat: file/folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import {
+    Blog,
+    Features,
+    Footer,
+    Header,
+    Possibility,
+    WhatGPT3,
+} from "./containers";
+
+import { Brand, CTA, NavBar } from "./components";
+
 const App = () => {
     return <div>gpt-3</div>;
 };
---
+++ src/containers/whatGPT3/WhatGPT3.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WhatGPT3 = () => {
+    return <div></div>;
+};
+
+export default WhatGPT3;
